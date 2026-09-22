2a73677de8610f09e6cc5c012b652f98bdea84b3
farias-madeiro <fariasmadeiro@gmail.com>
2026-09-22 02:57:11 -0300

    chore: atualizar versão para V7.26

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "br.com.controlequeijos"
         minSdk = 24
         targetSdk = 35
-        versionCode = 250
-        versionName = "7.25"
+        versionCode = 260
+        versionName = "7.26"
     }
 
     signingConfigs {
