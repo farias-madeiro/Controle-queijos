f730d3b78f9a904a3be12de3fdb07cf86ccebd46
farias-madeiro <fariasmadeiro@gmail.com>
2026-09-22 09:38:34 -0300

    chore: atualizar versao para V7.30

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "br.com.controlequeijos"
         minSdk = 24
         targetSdk = 35
-        versionCode = 290
-        versionName = "7.29"
+        versionCode = 300
+        versionName = "7.30"
     }
 
     signingConfigs {
