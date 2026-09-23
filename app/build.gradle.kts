070eabec54bfc367f50cf1b1d3c1b46893d4d62c
farias-madeiro <fariasmadeiro@gmail.com>
2026-09-23 14:12:14 -0300

    chore: atualizar versão para V7.42

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "br.com.controlequeijos"
         minSdk = 24
         targetSdk = 35
-        versionCode = 410
-        versionName = "7.41"
+        versionCode = 420
+        versionName = "7.42"
     }
 
     signingConfigs {
