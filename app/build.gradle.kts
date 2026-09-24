63ad6eccd84ef9beb9335a9d13f21f9595b151eb
farias-madeiro <fariasmadeiro@gmail.com>
2026-09-23 21:10:25 -0300

    chore: atualizar versão para V7.45

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "br.com.controlequeijos"
         minSdk = 24
         targetSdk = 35
-        versionCode = 440
-        versionName = "7.44"
+        versionCode = 450
+        versionName = "7.45"
     }
 
     signingConfigs {
