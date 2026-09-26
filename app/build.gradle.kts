36716d286d72b44a3083b0571d3f17e8ae40e1a2
farias-madeiro <fariasmadeiro@gmail.com>
2026-09-26 11:48:19 -0300

    chore: versionar aplicativo como V7.52

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "br.com.controlequeijos"
         minSdk = 24
         targetSdk = 35
-        versionCode = 510
-        versionName = "7.51"
+        versionCode = 520
+        versionName = "7.52"
         buildConfigField("String", "SUPABASE_URL", "\"https://vebrvocwgosaclbavqxf.supabase.co\"")
         buildConfigField("String", "SUPABASE_PUBLISHABLE_KEY", "\"sb_publishable_8XGCr-exfHkwcUu1DRm1RA_d8mT10Jf\"")
     }
